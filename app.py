486fcb754747699ad90d553be738ac8053fb694f
Andrew Hughson <kungfu.spike@gmail.com>
2012-04-10 21:39:49 +1200

    Add stuff

--- app.py
+++ app.py
@@ -1,13 +1,54 @@
 import os
+import tornado.ioloop
+import tornado.web
+import datetime
+import pymongo
 
-from flask import Flask
-app = Flask(__name__)
+from pymongo import Connection
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+config = {
+    'dev': True,
+    'mondodb_uri': os.environ.get('MONGOLAB_URI'),
+    'db_name': 'posts'
+}
+
+def get_database():
+    if config['dev'] == True:
+        try:
+            if config['dev'] == True:
+                connection = Connection()
+            else:
+                connection = Connection(config['mongodb_uri'])
+
+            database = connection[config['db_name']]
+        except:
+            print('Unable to connect to the database.')
+            return None
+
+    return database;
+
+
+class Post(object):
+    def __init__(self, title='No Title', author='Andrew Hughson', date=datetime.date.today()):
+        self.title = title
+        self.author = author
+        self.date = date
+        self.text = ''
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        
+        self.render('templates/index.html')
+
+settings = {
+    'static_path': os.path.join(os.path.dirname(__file__), 'static')
+    }
+        
+application = tornado.web.Application([
+    (r'/', MainHandler),
+], **settings)
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    application.listen(int(os.environ.get('PORT', 5000)))
+    tornado.ioloop.IOLoop.instance().start()
+
